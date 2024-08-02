0d9e305458b320d00fca299dc9e51bf89ee6bcbc
MoulinZ <zacharie.moulin@gmail.com>
2024-08-02 18:01:52 +0200

    modif environnement prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://socle.reserves-naturelles.org/api',
+    apiUrl: 'https://dev.socle2.reserves-naturelles.org/api',
     apiGeoNature: 'https://geonature.reserves-naturelles.org/api',
     token: '5cc129c8ceb208e18b23f6713624a4da'
   };
