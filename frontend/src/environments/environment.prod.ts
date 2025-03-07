d0be50b5b3846dbaddee65dd167fe932cadda84e
MoulinZ <zacharie.moulin@gmail.com>
2025-03-07 11:55:38 +0100

    suppression des fichiers environnements

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://socle.reserves-naturelles.org/api',
-  apiGeoNature: 'https://geonature.reserves-naturelles.org/api',
-  token: '5cc129c8ceb208e18b23f6713624a4da',
-  id_application: 11
-};
